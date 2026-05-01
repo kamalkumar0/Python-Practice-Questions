348e483f34bcbec56ddb493d3db5adceec4ba1bd
Kamal Kumar <214877569+kamalkumar0@users.noreply.github.com>
2026-05-01 19:51:05 +0530

    updated code in continue

--- continue.py
+++ continue.py
@@ -1,6 +1,6 @@
 #implementing for loop with range 20-30 & skip 25 with continue statement
 
-for num in range(20, 30+1):
-    if num == 25:
+for number in range(20, 30+1):
+    if number == 25:
         continue
-    print(num)
+    print(number)
